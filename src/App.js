d2818bb4f4b6c8847b5d2a9841d00ce7f7ba43b5
Sophia Koeut <koeutsophia@gmail.com>
2020-05-21 17:34:11 -0700

    added, edited company logo and banner img

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './images/logo.png'
+import banner from './images/banner.jpg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <div className="logo">
+          <img src={logo} alt="logo"></img>
+        </div>
+        <ul className="nav-links">
+          <li><a href="#">Why Saleswhale</a></li>
+          <li><a href="#">Product</a></li>
+          <li><a href="#">Company</a></li>
+        </ul>
+        <ul className="nav-links">
+          <li><a href="#">Log in</a></li>
+          <li><a href="#">Get a Demo</a></li>
+        </ul>
+      </nav>
+
+      <main>
+        <header>
+          <div className="top-banner">
+            <img src={banner} alt="fancy-banner"></img>
+          </div>
+        </header>
+
+
+
+
+      </main>
+
+
+
     </div>
   );
 }
