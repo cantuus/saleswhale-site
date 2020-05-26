9b19dce0ff7f55de7a449fac13dad7507a52968b
Sophia Koeut <koeutsophia@gmail.com>
2020-05-25 17:06:20 -0700

    IPAD: centered content in office cards, adjusted blue circle

--- src/components/OurOffices/OfficeCard.js
+++ src/components/OurOffices/OfficeCard.js
@@ -2,14 +2,11 @@ import React from 'react';
 
 export default function OfficeCard(props) {
     return (
-        <div className="outer-office-container">
-            <div className="inner-office-container">
-                <div className="office">
-                    <img src={props.img} alt="mail"></img>
-                    <h3>{props.location}</h3>
-                    <p>{props.address}</p>
-                </div>
-            </div>
+        <div className="office">
+            <img src={props.img} alt="mail"></img>
+            <h3>{props.location}</h3>
+            <p>{props.address}</p>
+            <p>{props.state} {props.zip}</p>
         </div>
     )
 }
--- src/components/OurOffices/OurOffices.js
+++ src/components/OurOffices/OurOffices.js
@@ -9,16 +9,24 @@ export default function OurOffices(props) {
     let officeStore = [
         {
             location: "Singapore",
-            address: "155B Telok Ayer Street Singapore, 068611"
+            address: "155B Telok Ayer Street",
+            state: "Singapore",
+            zip: "068611"
         },
         {
             location: "Washington DC",
-            address: "1701 Rhode Island Ave NW Washington, DC 20036"
+            address: "1701 Rhode Island Ave NW",
+            state: "Washington, DC",
+            zip: "20036"
         }
     ]
 
     return officeStore.map(office => {
 
-        return (<OfficeCard location={office.location} address={office.address} img={sinon} />);
+        return (<OfficeCard location={office.location} 
+            address={office.address} 
+            state={office.state}
+            zip={office.zip}
+            img={sinon} />);
     });
 }
